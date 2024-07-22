a51eda7cead01b4b8d4bb83f87df5706f0ae906a
kyukibug <alexknee44543@gmail.com>
2024-07-22 15:12:16 -0400

    WIP drafting get all categories

--- backend/handlers/category_handler.go
+++ backend/handlers/category_handler.go
@@ -1,15 +1,68 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 
+	"com.fukubox/database"
 	"github.com/go-chi/chi"
 )
 
+type Category struct {
+	id         int    `json:"id"`
+	user_id    int    `json:"user_id"`
+	name       string `json:"name"`
+	created_at string `json:"created_at"`
+	updated_at string `json:"updated_at"`
+}
+
 func GetCategories(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("GIVE ME MY CATEGORY GOPHER PLUSHIE"))
+	dbpool := database.GetDB()
+
+	user_id := r.Header.Get("userId")
+	if user_id == "" {
+		http.Error(w, "User ID is not set", http.StatusBadRequest)
+		return
+	}
+
+	conn, err := dbpool.Acquire(context.Background())
+	if err != nil {
+		http.Error(w, "Failed to acquire a database connection", http.StatusInternalServerError)
+		return
+	}
+	defer conn.Release()
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM categories WHERE user_id = $1", user_id)
+	if err != nil {
+		http.Error(w, "Query failed", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	categories := []Category{}
+
+	for rows.Next() {
+		var category Category
+		if err := rows.Scan(&category.id, &category.user_id, &category.name, &category.created_at, &category.updated_at); err != nil {
+			http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+			return
+		}
+		categories = append(categories, category)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, "Error after iterating rows", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(categories); err != nil {
+		http.Error(w, "Failed to encode response as JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func GetCategoriesById(w http.ResponseWriter, r *http.Request) {
