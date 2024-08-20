c420c02457ba20423f050dac75314a04b0ab00bc
kyukibug <alexknee44543@gmail.com>
2024-08-20 01:04:19 -0400

    adds more checks in auth middleware

--- backend/middleware/auth_middleware.go
+++ backend/middleware/auth_middleware.go
@@ -1,13 +1,24 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("userId") == "" {
-			http.Error(w, "User ID is not set", http.StatusBadRequest)
+		userHeader := r.Header.Get("userId")
+		if userHeader == "" {
+			log.Printf("userID Header not set")
+			http.Error(w, `Unauthorized (401)`, http.StatusUnauthorized)
+			return
+		}
+
+		_, err := strconv.Atoi(userHeader)
+		if err != nil {
+			log.Printf("Invalid `userId` Header: %v", userHeader)
+			http.Error(w, "Internal Server Error", http.StatusBadRequest)
 			return
 		}
 
